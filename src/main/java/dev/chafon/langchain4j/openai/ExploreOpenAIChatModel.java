ec17f8ee0768bc5bce7adba0808271990ade3b2c
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-08-16 17:39:32 +0530

    simple chat with open-ai chat model

---
+++ src/main/java/dev/chafon/langchain4j/openai/ExploreOpenAIChatModel.java
@@ -0,0 +1,26 @@
+package dev.chafon.langchain4j.openai;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+public class ExploreOpenAIChatModel {
+    public static void main(String[] args) {
+        String apiKey = System.getenv("OPENAI_API_KEY");
+        OpenAiChatModel model = OpenAiChatModel.withApiKey(apiKey);
+        simpleChatWithTheModel(model);
+    }
+
+    private static void simpleChatWithTheModel(ChatLanguageModel model) {
+        UserMessage firstUserMessage = UserMessage.from("Hello, my name is Chamil");
+        System.out.println(firstUserMessage.singleText());
+        AiMessage firstAiMessage = model.generate(firstUserMessage).content();
+        System.out.println(firstAiMessage.text());
+
+        UserMessage secondUserMessage = UserMessage.from("What is my name?");
+        System.out.println(secondUserMessage.singleText());
+        AiMessage secondAiMessage = model.generate(firstUserMessage, firstAiMessage, secondUserMessage).content();
+        System.out.println(secondAiMessage.text());
+    }
+}
